33d419ed24fc0dff20b8d93e14b454c2f81d720a
iccs-devs <support@iconceptcontactsolutions.com>
2016-09-29 04:52:06 +0800

    Update exam2.twig.php

--- app/views/exam2.twig.php
+++ app/views/exam2.twig.php
@@ -646,3 +646,17 @@
  
     </div>
 </div>
+
+<script>
+window.onbeforeunload = function (e) {
+  var message = "Your confirmation message goes here.",
+  e = e || window.event;
+  // For IE and Firefox
+  if (e) {
+    e.returnValue = message;
+  }
+
+  // For Safari
+  return message;
+};
+</script>
