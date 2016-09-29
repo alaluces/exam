9d6dbc803017285bf02e34faa696201759975691
iccs-devs <support@iconceptcontactsolutions.com>
2016-09-29 23:09:35 +0800

    Information for candidates intro exam edited

--- app/views/examIntro.twig.php
+++ app/views/examIntro.twig.php
@@ -15,16 +15,17 @@
                     
             <h3>Information for candidates</h3>    
             <ul>
-                <li>There are <b>four</b> parts to the test</li>
-                <li>You will hear each part <b>once</b> only</li>
-                <li>There are <b>40</b> questions</li>
-                <li>For each part of the test, there will be time for you to look through the questions and time for you to check the answers</li>
+                <li>There are <b>Two</b> test to the exam: <b>LISTENING AND ABSTRACT REASONING. </b> </li> 
+                <li>The first test is the listening exam which has <b>4</b>parts with <b>40</b>questions.</li> 
+                <li>The second test is the abstract reasoning with <b>20</b> questions.</li>
             </ul>             
             
             <br /><br />
-            Click <a onclick="return confirm('The exam will now start. Do you want to proceed?');" href="{{ URL.to('exam1') }}" class="btn btn-info" type="button">Here</a> to begin exam 1
+            Click <a onclick="return confirm('The exam will now start. Do you want to proceed?');" href="{{ URL.to('exam1') }}" class="btn btn-info" type="button">Here</a> to begin exam 1 
+            <b>(LISTENING EXAM) </b>
             <br /><br />
             Click <a onclick="return confirm('The exam will now start. Do you want to proceed?');" href="{{ URL.to('exam2') }}" class="btn btn-info" type="button">Here</a> to begin exam 2
+             <b>(ABSTRACT LISTENING) </b>
         </div>  
         <div class="col-md-4">            
         </div>       
